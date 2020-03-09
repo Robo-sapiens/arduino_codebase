2d44c98ddaa35fd5ae16fd8876638379f127e22b
sovadim <vadik.sofin@yandex.ru>
2020-03-09 23:23:50 +0300

    cppcheck now return 0

--- linters/run_cppcheck.sh
+++ linters/run_cppcheck.sh
@@ -1,1 +1,2 @@
-cppcheck `find -regex '\(.*[.]ino\)'` --enable=all --error-exitcode=1 --suppress=unusedFunction
+cppcheck `find -regex '\(.*[.]ino\)'` --enable=all --error-exitcode=1 --suppress=unusedFunction --error-exitcode=1
+echo "cppcheck done"
